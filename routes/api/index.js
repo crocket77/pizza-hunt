803ec94f37c03a9b85bef9802d2e43424a322ec1
crocket77 <ethanpfeiffer345@outlook.com>
2022-04-21 15:46:30 -0500

    finnished the comment model feature

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
+router.use('/comments', commentRoutes);
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
+
+
+// {
+//     "pizzaName": "The Demo Pizza",
+//     "createdBy": "Lernantino",
+//     "size": "Large",
+//     "toppings": [
+//         "Sausage",
+//         "Pepperoni",
+//         "Ricotta",
+//         "Hot Peppers"
+//     ]
+// }
+
+// {
+//     "writtenBy": "Amiko",
+//     "commentBody": "This pizza sounds great, but best pizza ever seems like a stretch."
+// }
